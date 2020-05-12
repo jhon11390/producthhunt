8970de966bf5d1086b4c37e326a356d3b789e8f8
jhon11390 <meljhon2@gmail.com>
2020-05-12 13:02:27 -0500

    Implementa las imágenes para los productos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_150344) do
+ActiveRecord::Schema.define(version: 2020_05_12_173100) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 2020_05_12_150344) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
